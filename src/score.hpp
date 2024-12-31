a31de7216b3cf5f358e6595db4a2e6f920a7244f
line <linefremery@gmail.com>
2024-12-31 14:25:42 +0100

    I will continue to improve it later

--- src/score.hpp
+++ src/score.hpp
@@ -8,6 +8,75 @@ __device__ __host__ float3 tothe4th(float3 x){
     return y*y;
 }
 
+__global__ void sumreduce(float3* dst, float3* src, int bl_x){
+    //dst must be of size 6*sizeof(float3)*blocknum at least
+    //shared memory needed is 6*sizeof(float3)*threadnum at least
+    const int x = threadIdx.x + blockIdx.x*blockDim.x;
+    const int thx = threadIdx.x;
+    const int threadnum = blockDim.x;
+    
+    extern __shared__ float3 sharedmem[];
+    float3* sumssim1 = sharedmem; //size sizeof(float3)*threadnum
+    float3* sumssim4 = sharedmem+blockDim.x; //size sizeof(float3)*threadnum
+    float3* suma1 = sharedmem+2*blockDim.x; //size sizeof(float3)*threadnum
+    float3* suma4 = sharedmem+3*blockDim.x; //size sizeof(float3)*threadnum
+    float3* sumd1 = sharedmem+4*blockDim.x; //size sizeof(float3)*threadnum
+    float3* sumd4 = sharedmem+5*blockDim.x; //size sizeof(float3)*threadnum
+
+    if (x >= bl_x){
+        sumssim1[thx].x = 0;
+        sumssim4[thx].x = 0;
+        suma1[thx].x = 0;
+        suma4[thx].x = 0;
+        sumd1[thx].x = 0;
+        sumd4[thx].x = 0;
+        
+        sumssim1[thx].y = 0;
+        sumssim4[thx].y = 0;
+        suma1[thx].y = 0;
+        suma4[thx].y = 0;
+        sumd1[thx].y = 0;
+        sumd4[thx].y = 0;
+        
+        sumssim1[thx].z = 0;
+        sumssim4[thx].z = 0;
+        suma1[thx].z = 0;
+        suma4[thx].z = 0;
+        sumd1[thx].z = 0;
+        sumd4[thx].z = 0;
+    } else {
+        sumssim1[thx] = src[x];
+        sumssim4[thx] = src[x + bl_x];
+        suma1[thx] = src[x + 2*bl_x];
+        suma4[thx] = src[x + 3*bl_x];
+        sumd1[thx] = src[x + 4*bl_x];
+        sumd4[thx] = src[x + 5*bl_x];
+    }
+    __syncthreads();
+    //now we need to do some pointer jumping to regroup every block sums;
+    int next = 1;
+    while (next < threadnum){
+        if (thx + next < threadnum && (thx%(next*2) == 0)){
+            sumssim1[thx] += sumssim1[thx+next];
+            sumssim4[thx] += sumssim4[thx+next];
+            suma1[thx] += suma1[thx+next];
+            suma4[thx] += suma4[thx+next];
+            sumd1[thx] += sumd1[thx+next];
+            sumd4[thx] += sumd4[thx+next];
+        }
+        next *= 2;
+        __syncthreads();
+    }
+    if (thx == 0){
+        dst[blockIdx.x] = sumssim1[0];
+        dst[gridDim.x + blockIdx.x] = sumssim4[0];
+        dst[2*gridDim.x + blockIdx.x] = suma1[0];
+        dst[3*gridDim.x + blockIdx.x] = suma4[0];
+        dst[4*gridDim.x + blockIdx.x] = sumd1[0];
+        dst[5*gridDim.x + blockIdx.x] = sumd4[0];
+    }
+}
+
 __global__ void allscore_map_Kernel(float3* dst, float3* im1, float3* im2, float3* mu1, float3* mu2, float3* s11, float3* s22, float3* s12, int width, int height){
     //dst must be of size 6*sizeof(float3)*blocknum at least
     //shared memory needed is 6*sizeof(float3)*threadnum at least
@@ -100,11 +169,22 @@ std::vector<float3> allscore_map(float3* im1, float3* im2, float3* mu1, float3* 
     for (int scale = 0; scale < 6; scale++){
         th_x = std::min((const int)(maxshared/(6*sizeof(float3)))/32*32, std::min(1024, w*h));
         bl_x = (w*h-1)/th_x + 1;
-        allscore_map_Kernel<<<dim3(bl_x), dim3(th_x), 6*sizeof(float3)*th_x, stream>>>(temp+scaleoutdone[scale], im1+index, im2+index, mu1+index, mu2+index, s11+index, s22+index, s12+index, w, h);
+        int blr_x = bl_x;
+        allscore_map_Kernel<<<dim3(bl_x), dim3(th_x), 6*sizeof(float3)*th_x, stream>>>(temp+scaleoutdone[scale]+((blr_x >= th_x) ? 6*bl_x : 0), im1+index, im2+index, mu1+index, mu2+index, s11+index, s22+index, s12+index, w, h);
         //printf("I got %s with %d\n", hipGetErrorString(hipGetLastError()), 6*sizeof(float3)*th_x);
         GPU_CHECK(hipGetLastError());
 
-        scaleoutdone[scale+1] = scaleoutdone[scale]+6*bl_x;
+        int oscillate = 0; //3 sets of memory: real destination at 0, first at 6*bl_x for oscillate 0 and last at 12*bl_x for oscillate 1;
+        int oldblr_x = bl_x;
+        while (blr_x >= th_x){
+            blr_x = (bl_x -1)/th_x + 1;
+            //sum reduce
+            sumreduce<<<dim3(blr_x), dim3(th_x), 6*sizeof(float3)*th_x, stream>>>(temp+scaleoutdone[scale]+((blr_x >= th_x) ? (oscillate+1)*6*bl_x : 0), temp+scaleoutdone[scale]+ (oscillate+1)*6*bl_x, oldblr_x);
+            oscillate ^= 1;
+            oldblr_x = blr_x;
+        }
+
+        scaleoutdone[scale+1] = scaleoutdone[scale]+6*blr_x;
         index += w*h;
         w = (w-1)/2+1;
         h = (h-1)/2+1;
