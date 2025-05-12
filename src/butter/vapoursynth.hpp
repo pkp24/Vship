41d42ec775bb38218a05ea795b8db0615caa6d6b
line <linefremery@gmail.com>
2025-05-12 11:28:45 +0200

    forgot to also fix in butter

--- src/butter/vapoursynth.hpp
+++ src/butter/vapoursynth.hpp
@@ -191,7 +191,7 @@ namespace butter{
         }
     
         d.streamnum = std::min(d.streamnum, infos.numThreads);
-        d.streamnum = std::min(d.streamnum, (int)(devattr.totalGlobalMem/(31*4*viref->width*viref->height))); //VRAM overcommit partial protection.
+        d.streamnum = std::min((size_t)d.streamnum, (size_t)((size_t)devattr.totalGlobalMem/(32llu*4llu*(size_t)viref->width*(size_t)viref->height))); //VRAM overcommit partial protection
         d.streamnum = std::max(d.streamnum, 1);
         d.streams = (hipStream_t*)malloc(sizeof(hipStream_t)*d.streamnum);
         for (int i = 0; i < d.streamnum; i++){
