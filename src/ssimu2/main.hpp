855986dacbf210637314730aab09316a77978915
line <linefremery@gmail.com>
2025-05-12 19:21:58 +0200

    fix ssimu2 bug

--- src/ssimu2/main.hpp
+++ src/ssimu2/main.hpp
@@ -38,7 +38,7 @@ double ssimu2process(const uint8_t *srcp1[3], const uint8_t *srcp2[3], float3* p
     int64_t whs[6] = {wh, ((height-1)/2 + 1)*((width-1)/2 + 1), ((height-1)/4 + 1)*((width-1)/4 + 1), ((height-1)/8 + 1)*((width-1)/8 + 1), ((height-1)/16 + 1)*((width-1)/16 + 1), ((height-1)/32 + 1)*((width-1)/32 + 1)};
     int64_t whs_integral[7];
     whs_integral[0] = 0;
-    for (int i = 0; i < 7; i++){
+    for (int i = 0; i < 6; i++){
         whs_integral[i+1] = whs_integral[i] + whs[i];
     }
     int64_t totalscalesize = whs_integral[6];
