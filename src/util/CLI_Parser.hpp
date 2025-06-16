be6205dbf528e748da292d00b48063ad6d47444b
line <linefremery@gmail.com>
2025-06-16 19:30:56 +0200

    alias_map is now returning an index instead of storing and duplicating the pointer

--- src/util/CLI_Parser.hpp
+++ src/util/CLI_Parser.hpp
@@ -11,11 +11,15 @@ struct ArgParser {
     using TargetVariant = std::variant<bool*, int*, std::string*>;
 
     struct FlagGroup {
-        std::vector<std::string> aliases; TargetVariant target; std::string help_text;
+        bool set = false;
+        std::vector<std::string> aliases; 
+        TargetVariant target; 
+        std::string help_text;
     };
 
     std::vector<FlagGroup> flag_groups;
-    std::map<std::string, TargetVariant> alias_map;
+    std::map<std::string, int> alias_map; //returns index in flag_groups
+    std::vector<int> positional_indexing;
 
     ArgParser() {
         add_flag({"-h", "--help"}, &show_help_flag, "Display this help message");
@@ -23,14 +27,15 @@ struct ArgParser {
 
     template<typename TargetType>
     void add_flag(const std::vector<std::string>& flag_names, TargetType* target_pointer,
-                  std::string help_description = "") {
+                  std::string help_description = "", bool positional = false) {
         static_assert(std::is_same_v<TargetType, bool> || std::is_same_v<TargetType, int> ||
             std::is_same_v<TargetType, std::string>, "Unsupported flag type"
         );
 
-        flag_groups.push_back({flag_names, target_pointer, help_description});
-        const auto& group = flag_groups.back();
-        for (const std::string& name : flag_names) alias_map[name] = group.target;
+        for (const std::string& name : flag_names) alias_map[name] = flag_groups.size(); //new index
+        if (positional) positional_indexing.push_back(flag_groups.size());
+        flag_groups.push_back({false, flag_names, target_pointer, help_description});
+        //const auto& group = flag_groups.back();
     }
 
     // Takes in a vector of string from the cli to parse, returns a 0 if sucessfull
@@ -53,14 +58,16 @@ private:
     // Parse a flag at index in 'arguments', returns false if unknown or invalid
     bool parse_flag(const std::vector<std::string>& arguments, size_t& index) const {
         const std::string& flag = arguments[index];
-        auto found = alias_map.find(flag);
-        if (found == alias_map.end()) {
+        auto foundIndexIterator = alias_map.find(flag);
+        if (foundIndexIterator == alias_map.end()) {
             std::cerr << "Unknown argument: " << flag << "\n";
             return false;
         }
+
+        auto& found = flag_groups[foundIndexIterator->second].target;
     
-        if (std::holds_alternative<bool*>(found->second)) {
-            bool* ptr = std::get<bool*>(found->second);
+        if (std::holds_alternative<bool*>(found)) {
+            bool* ptr = std::get<bool*>(found);
             *ptr = !*ptr;
             return true;
         }
@@ -70,14 +77,14 @@ private:
             return false;
         }
     
-        if (std::holds_alternative<std::string*>(found->second)) {
-            std::string* ptr = std::get<std::string*>(found->second);
+        if (std::holds_alternative<std::string*>(found)) {
+            std::string* ptr = std::get<std::string*>(found);
             *ptr = arguments[++index];
             return true;
         }
     
         // Assume it is an int
-        int* ptr = std::get<int*>(found->second);
+        int* ptr = std::get<int*>(found);
         try {
             *ptr = std::stoi(arguments[++index]);
             return true;
