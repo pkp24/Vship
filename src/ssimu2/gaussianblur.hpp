7b8f4fd1f6ebbece0befaead3cbd4d083f2eacd2
line <linefremery@gmail.com>
2025-07-06 23:48:52 +0200

    trying to fix back the ssimu2 speed regression

--- src/ssimu2/gaussianblur.hpp
+++ src/ssimu2/gaussianblur.hpp
@@ -14,6 +14,7 @@ public:
     
         hipMalloc(&gaussiankernel_d, sizeof(float)*(4*GAUSSIANSIZE+3));
         hipMemcpyHtoD((hipDeviceptr_t)gaussiankernel_d, gaussiankernel, (4*GAUSSIANSIZE+3)*sizeof(float));
+        gaussiankernel_integral_d = gaussiankernel_d + 2*GAUSSIANSIZE+1;
     }
     void destroy(){
         hipFree(gaussiankernel_d);
@@ -60,14 +61,15 @@ __device__ void GaussianSmart_Device(float3* tampon, int64_t x, int64_t y, int64
     //horizontalBlur on tampon restraint into rectangle [8 - 24][0 - 32] -> 2 pass per thread
 
     //1st pass in [8 - 24][0 - 16]
-    float tot = 0.;
+    float tot;
     float3 out = makeFloat3(0.f, 0.f, 0.f);
     float3 out2 = makeFloat3(0.f, 0.f, 0.f);
+    //border handling precompute
+    int beg = max((long)0, x-8)-(x-8);
+    int end2 = min(width, x+9)-(x-8);
+    tot = gaussiankernel_integral[end2] - gaussiankernel_integral[beg];
     for (int i = 0; i < 17; i++){ //starting 8 to the left and going 8 to the right
         out += tampon[thy*32 + thx+i]*gaussiankernel[i];
-
-        //border handling precompute
-        if (tampon_base_x+thx+i >= 0 && tampon_base_x+thx+i < width) tot += gaussiankernel[i];
     }
 
     //2nd pass in [8 - 24][16 - 32]
@@ -82,12 +84,11 @@ __device__ void GaussianSmart_Device(float3* tampon, int64_t x, int64_t y, int64
 
     //verticalBlur on tampon restraint into rectangle [8 - 24][8 - 24] -> 1 pass per thread
     out = makeFloat3(0.f, 0.f, 0.f);
-    tot = 0.;
+    beg = max((long)0, y-8)-(y-8);
+    end2 = min(height, y+9)-(y-8);
+    tot = gaussiankernel_integral[end2] - gaussiankernel_integral[beg];
     for (int i = 0; i < 17; i++){ //starting 8 to the left and going 8 to the right
         out += tampon[(thy+i)*32 + thx+8]*gaussiankernel[i];
-
-        //border handling precompute
-        if (tampon_base_y+thy+i >= 0 && tampon_base_y+thy+i < height) tot += gaussiankernel[i];
     }
 
     __syncthreads();
