5533a917fd7477c003d6fd4a30c3add557dcd298
line <linefremery@gmail.com>
2025-09-27 13:39:43 +0200

    change clock type for time measure

--- test/apibenchmark.cpp
+++ test/apibenchmark.cpp
@@ -62,16 +62,16 @@ int main(){
     //first initilization time measure
     std::cout << "--------------------API-Testing-----------------------" << std::endl;
     int numgpu;
-    auto time1 = std::chrono::system_clock::now();
+    auto time1 = std::chrono::high_resolution_clock::now();
     Vship_GetDeviceCount(&numgpu);
-    auto time2 = std::chrono::system_clock::now();
+    auto time2 = std::chrono::high_resolution_clock::now();
     Vship_GetDeviceCount(&numgpu);
-    auto time3 = std::chrono::system_clock::now();
+    auto time3 = std::chrono::high_resolution_clock::now();
     std::cout << "Cuda/HIP initialization time: " << (double)((time2 - time1).count() - (time3 - time2).count())/1000000000. << "s" << std::endl;
     std::cout << "Check Device Count time: " << (time3 - time2).count()/1000000000. << "s" << std::endl;
-    time1 = std::chrono::system_clock::now();
+    time1 = std::chrono::high_resolution_clock::now();
     Vship_GPUFullCheck(0);
-    time2 = std::chrono::system_clock::now();
+    time2 = std::chrono::high_resolution_clock::now();
     std::cout << "GPUFullCheck time: " << (time2 - time1).count()/1000000000. << "s" << std::endl;
 
     std::cout << "------------------SSIMU2-Testing-----------------------" << std::endl;
@@ -80,18 +80,18 @@ int main(){
     Vship_SSIMU2Handler ssimu2handler2;
     Vship_SSIMU2Handler ssimu2handler3;
     Vship_SSIMU2Init(&ssimu2handler, width, height);
-    time2 = std::chrono::system_clock::now();
+    time2 = std::chrono::high_resolution_clock::now();
     Vship_SSIMU2Init(&ssimu2handler2, width, height);
     Vship_SSIMU2Init(&ssimu2handler3, width, height);
-    time3 = std::chrono::system_clock::now();
+    time3 = std::chrono::high_resolution_clock::now();
     std::cout << "Preprocess SSIMU2 Handler time (lantency + process): " << (time3 - time2).count()/2000000000. << "s" << std::endl;
 
     double score;
-    time1 = std::chrono::system_clock::now();
+    time1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 300; i++){
         Vship_ComputeSSIMU2Uint16(ssimu2handler, &score, original.csrcp, other.csrcp, stride);
     }
-    time2 = std::chrono::system_clock::now();
+    time2 = std::chrono::high_resolution_clock::now();
     std::thread thread1([&]{
         for (int i = 0; i < 100; i++){
             Vship_ComputeSSIMU2Uint16(ssimu2handler, &score, original.csrcp, other.csrcp, stride);
@@ -110,17 +110,17 @@ int main(){
     thread1.join();
     thread2.join();
     thread3.join();
-    time3 = std::chrono::system_clock::now();
+    time3 = std::chrono::high_resolution_clock::now();
     std::cout << "SSIMU2 Process Uint16 time ((latency + process)*300/300): " << (time2 - time1).count()/300000000000. << "s" << std::endl;
     std::cout << "SSIMU2 Process Uint16 time (latency + process*3)*100/300: " << (time3 - time2).count()/300000000000. << "s" << std::endl;
     std::cout << "=> SSIMU2 Process Uint16 \"process time\": " << - 0.5*(time2 - time1).count()/300000000000. + 3.*0.5*(time3 - time2).count()/300000000000. << "s" << std::endl;
     std::cout << "=> SSIMU2 Process Uint16 \"latency time\": " << 3.*0.5*(time2 - time1).count()/300000000000. - 3.*0.5*(time3 - time2).count()/300000000000. << "s" << std::endl;
 
     Vship_SSIMU2Free(ssimu2handler);
-    time2 = std::chrono::system_clock::now();
+    time2 = std::chrono::high_resolution_clock::now();
     Vship_SSIMU2Free(ssimu2handler2);
     Vship_SSIMU2Free(ssimu2handler3);
-    time3 = std::chrono::system_clock::now();
+    time3 = std::chrono::high_resolution_clock::now();
     std::cout << "Free SSIMU2 Handler time (latency + process): " << (time3 - time2).count()/2000000000. << "s" << std::endl;
 
 
@@ -130,18 +130,18 @@ int main(){
     Vship_ButteraugliHandler butterhandler2;
     Vship_ButteraugliHandler butterhandler3;
     Vship_ButteraugliInit(&butterhandler, width, height, 80.);
-    time2 = std::chrono::system_clock::now();
+    time2 = std::chrono::high_resolution_clock::now();
     Vship_ButteraugliInit(&butterhandler2, width, height, 80.);
     Vship_ButteraugliInit(&butterhandler3, width, height, 80.);
-    time3 = std::chrono::system_clock::now();
+    time3 = std::chrono::high_resolution_clock::now();
     std::cout << "Preprocess Butteraugli Handler time (latency + process): " << (time3 - time2).count()/2000000000. << "s" << std::endl;
 
     Vship_ButteraugliScore scorebutter;
-    time1 = std::chrono::system_clock::now();
+    time1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 300; i++){
         Vship_ComputeButteraugliUint16(butterhandler3, &scorebutter, NULL, 0, original.csrcp, other.csrcp, stride);
     }
-    time2 = std::chrono::system_clock::now();
+    time2 = std::chrono::high_resolution_clock::now();
     std::thread thread4([&]{
         for (int i = 0; i < 100; i++){
             Vship_ComputeButteraugliUint16(butterhandler3, &scorebutter, NULL, 0, original.csrcp, other.csrcp, stride);
@@ -160,17 +160,17 @@ int main(){
     thread4.join();
     thread5.join();
     thread6.join();
-    time3 = std::chrono::system_clock::now();
+    time3 = std::chrono::high_resolution_clock::now();
     std::cout << "Butter Process Uint16 time ((latency + process)*300/300): " << (time2 - time1).count()/300000000000. << "s" << std::endl;
     std::cout << "Butter Process Uint16 time (latency + process*3)*100/300: " << (time3 - time2).count()/300000000000. << "s" << std::endl;
     std::cout << "=> Butter Process Uint16 \"process time\": " << - 0.5*(time2 - time1).count()/300000000000. + 3.*0.5*(time3 - time2).count()/300000000000. << "s" << std::endl;
     std::cout << "=> Butter Process Uint16 \"latency time\": " << 3.*0.5*(time2 - time1).count()/300000000000. - 3.*0.5*(time3 - time2).count()/300000000000. << "s" << std::endl;
 
     Vship_ButteraugliFree(butterhandler);
-    time2 = std::chrono::system_clock::now();
+    time2 = std::chrono::high_resolution_clock::now();
     Vship_ButteraugliFree(butterhandler2);
     Vship_ButteraugliFree(butterhandler3);
-    time3 = std::chrono::system_clock::now();
+    time3 = std::chrono::high_resolution_clock::now();
     std::cout << "Free Butteraugli Handler time (latency + process): " << (time3 - time2).count()/2000000000. << "s" << std::endl;
     return 0;
 }
